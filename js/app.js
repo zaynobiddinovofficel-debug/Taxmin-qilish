bf54e9f7106d50aaac93a0bf8d1e6de261008f1f
biloliddin <zaynobiddinovofficel@gmail.com>
2025-11-12 10:23:57 -1100

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,27 @@
+
+// let soz = prompt("So‘z kiriting:");
+
+// console.log(soz[0]);
+// console.log(soz[1]);
+// console.log(soz[2]);
+// console.log(soz[3]);
+// console.log(soz[4]);
+// console.log(soz[5]);
+// console.log(soz[6]);
+// console.log(soz[7]);
+// console.log(soz[8]);
+// console.log(soz[9]);
+// console.log(soz[10]);
+
+// while(true) {
+//     let min = 44;
+//     let max = 67;
+//     let randomNum = Math.trunc(Math.random() * (max - min + 1) + min);
+//     let userprompt = prompt("Siz o'ylagan shumi: " + randomNum);
+    
+//     if (userprompt === "ha") {
+//         console.log("Shu!");
+//         break;
+//     }
+// }
+
