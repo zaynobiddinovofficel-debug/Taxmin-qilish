08d1dfef10b4f051ce03f83d613a46e26c40dfcf
biloliddin <zaynobiddinovofficel@gmail.com>
2025-11-12 13:36:49 -1100

    Initial commit

--- js/app.js
+++ js/app.js
@@ -13,6 +13,13 @@
 // console.log(soz[9]);
 // console.log(soz[10]);
 
+// ]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
+// let bu o'garuvchi bu kiritilgan so'zni saqlaydi
+//  prompt esa foydalanuvchidan soz' so'raydi 
+//  console.log esa kiritilgan sozni oynada chiqaradi
+// ]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
+
+
 // while(true) {
 //     let min = 44;
 //     let max = 67;
@@ -24,4 +31,16 @@
 //         break;
 //     }
 // }
+
+
+
+// ]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
+// while - bu cheksiz aylantirib beradi true yoki false bo'lmaguncha
+// min-max raqamlar orasi da chegara
+// Math.random-0.  1 gacha bo'gan sonlarni tasodifiy chiqarib beradi
+// Math.trunc esa tasodifiy sonlarni butunlab beradi yani yaxlitlab berai verguldan yoki nuqtadan keying sonlarni olib tashadi
+// randomNum esa 44   67 bogan sonarni chiqarib beradi
+// prompt esa foydalanucvhi javob beradi ha yoki yoq so'z soraydi kiritadi
+// break esa while toxtadi
+// ]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
 
